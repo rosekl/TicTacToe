66be6fc1d4473831c1e150dcb56f2648fa79e24c
Katie Forshier <katier@slalom.com>
2015-04-13 22:20:57 -0500

    Initial commit
    Initial commit

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol TicTacToeProtocol {
+    func GetSquareMark(square: TicTacToeSquare) -> SquareMark
+    func SetSquareMark(square: TicTacToeSquare, mark: SquareMark) -> Void
+    func IsWinningLinePresent() -> Bool
+}
 
+public class TicTacToeSquare {
+    var row: Int
+    var column: Int
+    
+    init(row: Int, column: Int) {
+        self.row = row
+        self.column = column
+    }
+}
+
+public enum SquareMark {
+    case X
+    case O
+    case BLANK
+}
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var square1: UIButton!
+    @IBOutlet weak var square2: UIButton!
+    @IBOutlet weak var square3: UIButton!
+    @IBOutlet weak var square4: UIButton!
+    @IBOutlet weak var square5: UIButton!
+    @IBOutlet weak var square6: UIButton!
+    @IBOutlet weak var square7: UIButton!
+    @IBOutlet weak var square8: UIButton!
+    @IBOutlet weak var square9: UIButton!
+    
+    @IBOutlet var gameSquares: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    //TODO implement protocol
 
 }
 
